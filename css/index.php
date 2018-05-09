009d73297c7743ba2da6f8d15413c6d9c93f4184
Daniels-PC\Daniels <daniel_import@hotmail.com>
2018-05-08 23:01:42 -0300

    pastas

---
+++ css/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+ <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png"/>
+ <link href="css/style.css" rel="stylesheet" type="text/css"/>
+
+
+
+<title>:::SiFoa:::</title>
+</head>
+
+<body>
+
+
+</body>
+
+</html>
