ef7cce393a5f52cbbf6a7dde9e39dfeccd0bfe48
Daniels-PC\Daniels <daniel_import@hotmail.com>
2018-05-23 00:13:07 -0300

    att dia 23 de maio

---
+++ bk/turmas.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+ <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png"/>
+ <link href="css/style.css" rel="stylesheet" type="text/css"/>
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+
+<title>Sistema de Informação</title>
+</head>
+
+<body>
+<div class="header">
+
+<div class="menu">
+<img  class="logo" src="images/logoteste.png">
+	
+<ul>
+		<li><a href="index.html">Home</a></li>
+		<li><a href="materias.html">Matérias</a></li>
+		<li><a href="turmas.html">Turmas</a></li>
+	</ul>
+
+
+
+
+
+
+
+	</div>
+		<h1>SISTEMA DE INFORMAÇÃO</h1>
+</div>
+
+
+<div class="info-t">
+
+	<h1>Curriculos</h1>
+	<div class="borda"></div>
+
+		<div class="t-a">
+
+			<h3>Turma A</h3>
+			<div class="borda"></div>
+
+			<div class="curriculo"><a href="curriculos/turma_a/julia/index.html" target="_blank"><img src="curriculos/turma_a/julia/foto.png" alt="curriculos"></a></div>
+			<div class="curriculo"><a href="curriculos/turma_a/lindomar/index.php" target="_blank"><img src="curriculos/turma_a/lindomar/img/eu.jpg" alt="curriculos"></a></div>
+			<div class="curriculo"></div>
+			<div class="curriculo"></div>
+
+
+
+		</div>
+
+		<div class="t-b">
+			<h3>Turma B</h3>
+			<div class="borda"></div>
+
+			<div class="curriculo"><a href="curriculos/turma_b/bruno/index.html" target="_blank"><img src="curriculos/turma_b/bruno/Bruno.png" alt="curriculos"></a></div>
+			<div class="curriculo"><a href="curriculos/turma_b/gabriel/index.html" target="_blank"><img src="curriculos/turma_b/gabriel/mfots.png" alt="curriculos"></a></div>
+			<div class="curriculo"><a href="curriculos/turma_b/hugo/index.html" target="_blank"><img src="curriculos/turma_b/hugo/perfil.jpg" alt="curriculos"></a></div>
+			<div class="curriculo"><a href="curriculos/turma_b/jp/index.html" target="_blank"><img src="curriculos/turma_b/jp/foto.jpg" alt="curriculos"></a></div>
+			<div class="curriculo"><a href="curriculos/turma_b/leo/index.html" target="_blank"><img src="curriculos/turma_b/leo/eu.jpg" alt="curriculos"></a></div>
+			<div class="curriculo"><a href="curriculos/turma_b/luorhan/index.html" target="_blank"><img src="curriculos/turma_b/luorhan/luorhan1.jpg" alt="curriculos"></a></div>
+			<div class="curriculo"><a href="curriculos/turma_b/miguel/index.html" target="_blank"><img src="curriculos/turma_b/miguel/foto.jpeg" alt="curriculos"></a></div>
+			<div class="curriculo"><a href="curriculos/turma_b/pedro_loos/index.html" target="_blank"><img src="curriculos/turma_b/" alt="Pedro_Loos"></a></div>
+			<div class="curriculo"><a href="curriculos/turma_b/roberto/index.html" target="_blank"><img src="curriculos/turma_b/roberto/roberto.jpg" alt="curriculos"></a></div>
+			<div class="curriculo"><a href="curriculos/turma_b/rogerio/index.html" target="_blank"><img src="curriculos/turma_b/rogerio/eu.png" alt="curriculos"></a></div>
+			<div class="curriculo"><a href="curriculos/turma_b/saintclair/index.html" target="_blank"><img src="curriculos/turma_b/saintclair/" alt="Saint Clair"></a></div>
+			
+			
+
+
+
+
+
+
+
+		</div>
+
+
+</div>
+
+
+<div class="rodape">
+<p>footer</p>
+
+
+</div>
+
+
+
+
+</body>
+
+</html>
