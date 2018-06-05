88458a0f3cc34028838041041b0dae04e1bdfaa3
vsgaster <39140415+vsgaster@users.noreply.github.com>
2018-06-05 00:40:38 -0300

    Add files via upload
    pasta de curriculos da turma a

---
+++ turma_a/lindomar/index.php
@@ -0,0 +1,311 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Meu curriculo</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Baloo+Chettan" rel="stylesheet">
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</head>
+<body>
+	<header>
+
+		<div class="container">
+			<div class="row">
+				<div id="logo-img" class="col-md-3">
+					<img src="img/eu.jpg">
+				</div>	
+				<div id="meu-nome" class="col-md-6 offset-md-1" >
+					<h1>Lindomar</h1>
+					<h3>Silva Gonçalves</h3>
+				</div>	
+			</div>
+		</div>
+	</header>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-5">
+				<h2>Formação Acadêmica</h2>
+				<hr>
+
+				<div class="row">
+					<div class="ponteiro col-md-1">
+						<img src="img/ponteiro.png" height="120px;">
+					</div>
+					<div class="col-md-11">
+						<h4>Sistemas de Informação</h4>
+						<p>UIFOA - Volta Redonda</p>
+						<p>Término previsto para 2021</p>					
+					</div>					
+				</div>
+
+
+			</div>
+
+			<div class="col-md-5 offset-md-2">
+				<h2>Experiência</h2>
+				<hr>
+
+				<div class="row">
+					<div class="ponteiro col-md-1">
+						<img src="img/ponteiro.png" height="120px;">
+					</div>
+					<div class="col-md-11">
+						<h4>Eletricista de Manutenção Industrial</h4>
+						<p>Polimar Industria e Comércio de Produtos Plásticos</p>
+						<p>2014 - 2016</p>					
+					</div>					
+				</div>
+			</div>				
+		</div>
+		<hr class="separador">
+		<div class="row">
+			<div class="col-md-12">
+				<h2 style="text-align: center">Cursos</h2>
+				<hr>
+			</div>
+
+			<div class="col-md-6">
+				<div class="row">
+
+					<div class="ponteiro col-md-1">
+						<img src="img/ponteiro.png" height="120px;">
+					</div>
+					<div class="col-md-11">
+						<h4>Eletricista de Manutenção Industrial</h4>
+						<p>SENAI - Valença |  11/2014 – 09/2016 </p>
+						
+						<p>Carga horária: 800 horas</p>					
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="row">
+
+					<div class="ponteiro col-md-1">
+						<img src="img/ponteiro.png" height="120px;">
+					</div>
+					<div class="col-md-11">
+						<h4>Programador de Sistemas</h4>
+						<p>SENAI - Valença | 07/2017 – 12/2017 </p>
+						
+						<p>Carga horária: 200 horas horas</p>					
+					</div>
+
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="row">
+
+					<div class="ponteiro col-md-1">
+						<img src="img/ponteiro.png" height="120px;">
+					</div>
+					<div class="col-md-11">
+						<h4>Curso Completo de PHP 7</h4>
+						<p><a href="https://www.udemy.com.br">Udemy</a> |  Concluído em: 02/2018 </p>
+						
+						<p>Carga horária: 33 horas</p>					
+					</div>
+
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="row">
+
+					<div class="ponteiro col-md-1">
+						<img src="img/ponteiro.png" height="120px;">
+					</div>
+					<div class="col-md-11">
+						<h4>Lógica de Programação</h4>
+						<p><a href="https://www.softblue.com.br/">Softblue</a> |  Concluído em: 08/2017 </p>
+						
+						<p>Carga horária: 20 horas</p>					
+					</div>
+
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="row">
+
+					<div class="ponteiro col-md-1">
+						<img src="img/ponteiro.png" height="120px;">
+					</div>
+					<div class="col-md-11">
+						<h4>Curso completo de Desenvolvimento Web</h4>
+						<p><a href="https://www.udemy.com.br">Udemy</a> |  Concluído em: 02/2018 </p>
+						
+						<p>Carga horária: 34,5 horas</p>					
+					</div>
+
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="row">
+
+					<div class="ponteiro col-md-1">
+						<img src="img/ponteiro.png" height="120px;">
+					</div>
+					<div class="col-md-11">
+						<h4>Empreendedorismo</h4>
+						<p>SENAI-RJ |  Concluído em: 11/2015 </p>
+						
+						<p>Carga horária: 14 horas</p>					
+					</div>
+
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="row">
+
+					<div class="ponteiro col-md-1">
+						<img src="img/ponteiro.png" height="120px;">
+					</div>
+					<div class="col-md-11">
+						<h4>Lógica de Programação</h4>
+						<p> SENAI-RJ |  Concluído em: 11/2015 </p>
+						
+						<p>Carga horária: 14 horas</p>					
+					</div>
+
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="row">
+
+					<div class="ponteiro col-md-1">
+						<img src="img/ponteiro.png" height="120px;">
+					</div>
+					<div class="col-md-11">
+						<h4>Tecnologia da Informação e Comunicação</h4>
+						<p>SENAI-RJ |  Concluído em: 11/2015 </p>
+						
+						<p>Carga horária: 14 horas</p>					
+					</div>
+
+				</div>
+			</div>
+		</div>
+		<hr class="separador">
+		<div class="row">
+			<div class="col-md-7">
+				<h2>Objetivo</h2>
+				<hr>
+				<div class="row">
+					<div class="ponteiro col-md-1">
+						<img src="img/ponteiro.png" height="120px;">
+					</div>
+					<div class="col-md-11"><br>
+						<p>Estou à procura da minha primeira experiência profissional na área de Tecnologia da Informação
+						com o intuito de aprimorar os meus conhecimentos e elevar a qualidade dos serviços prestados pela
+						empresa.</p>
+					</div>
+				</div>	
+			</div>
+			<div class="col-md-5">
+				<h2>Habilidades Profissionais</h2>
+				<hr>
+				<div class="row">
+					<div class="col-md-8">
+						<p class="fontH">HTML</p>
+					</div>
+					<div class="col-md-4" >
+						<img src="img/ponto.png" height="20px;">
+						<img src="img/ponto.png" height="20px;">
+						<img src="img/ponto.png" height="20px;">
+						<img src="img/ponto.png" height="20px;">
+						<img src="img/ponto.png" height="20px;">
+					</div>
+				</div>	
+				<div class="row">
+					<div class="col-md-8">
+						<p class="fontH">PHP</p>
+					</div>
+					<div class="col-md-4">
+						<img src="img/ponto.png" height="20px;">
+						<img src="img/ponto.png" height="20px;">
+						<img src="img/ponto.png" height="20px;">
+						<img src="img/ponto.png" height="20px;">
+						<img src="img/ponto.png" height="20px;" class="ponto-cinza">
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-8">
+						<p class="fontH">JAVASCRIPT</p>
+					</div>
+					<div class="col-md-4">
+						<img src="img/ponto.png" height="20px;">
+						<img src="img/ponto.png" height="20px;">
+						<img src="img/ponto.png" height="20px;">
+						<img src="img/ponto.png" height="20px;" class="ponto-cinza">
+						<img src="img/ponto.png" height="20px;" class="ponto-cinza">
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-7">
+				<h2>Contato</h2>
+				<hr>
+				<div class="row">
+					<div class="ponteiro col-md-1">
+						<img src="img/ponteiro.png" height="120px;">
+					</div>
+					<div class="col-md-11">
+						<p><b>Endereço: </b>Rodovia RJ 145, número 26364, Canteiro, Valença -RJ</p>
+						<p><b>Email: </b>lindomar1016@hotmail.com | lindomar1017@hotmail.com</p>
+						<p><b>Telefone: </b>(24)998734138 | (24) 2453-5395 | (24) 2453-5455</p>
+					</div>
+				</div>	
+			</div>	
+
+			<div class="col-md-5">
+				<h2>Habilidades Pessoais</h2>
+				<hr>
+				<div class="row">
+					<div class="col-md-8">
+						<p class="fontH">COMUNICAÇÃO</p>
+					</div>
+					<div class="col-md-4">
+						<img src="img/ponto.png" height="20px;" >
+						<img src="img/ponto.png" height="20px;">
+						<img src="img/ponto.png" height="20px;" >
+						<img src="img/ponto.png" height="20px;">
+						<img src="img/ponto.png" height="20px;">
+					</div>
+				</div>	
+				<div class="row">
+					<div class="col-md-8">
+						<p class="fontH">LIDERANÇA</p>
+					</div>
+					<div class="col-md-4">
+						<img src="img/ponto.png" height="20px;">
+						<img src="img/ponto.png" height="20px;">
+						<img src="img/ponto.png" height="20px;">
+						<img src="img/ponto.png" height="20px;" >
+						<img src="img/ponto.png" height="20px;" class="ponto-cinza">
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-8">
+						<p class="fontH">COLABORAÇÃO</p>
+					</div>
+					<div class="col-md-4">
+						<img src="img/ponto.png" height="20px;">
+						<img src="img/ponto.png" height="20px;">
+						<img src="img/ponto.png" height="20px;">
+						<img src="img/ponto.png" height="20px;">
+						<img src="img/ponto.png" height="20px;" >
+					</div>
+				</div>
+		
+			</div>
+		</div>
+	</div>
+	<footer>
+		<p>Copyright &copy Lindomar Silva Gonçalves - 2018 | Todos os direitos reservados</p>
+	</footer>
+
+
+</body>
+</html>
